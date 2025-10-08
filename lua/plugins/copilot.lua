647560731b523eeb7aecbd52fa7c8ffee0e1d2e7
kawaji-r <kawaji.r.333@gmail.com>
2025-10-08 13:17:13 +0900

    add copilot-chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,19 @@
 local M = {}
 
 function M.plugin_spec()
-    return { { 'github/copilot.vim' } }
+    return {
+        { 'github/copilot.vim' },
+        {
+            "CopilotC-Nvim/CopilotChat.nvim",
+            dependencies = {
+                { "nvim-lua/plenary.nvim", branch = "master" },
+            },
+            build = "make tiktoken",
+            opts = {
+                -- See Configuration section for options
+            },
+        }
+    }
 end
 
 return M
